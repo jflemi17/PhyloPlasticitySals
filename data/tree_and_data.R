850647446ff8a4b45d69babb2911cc0b03a27616
jflemi17 <jflemi17@vols.utk.edu>
2021-05-07 17:47:28 -0400

    final project

--- data/tree_and_data.R
+++ data/tree_and_data.R
@@ -1,18 +1,27 @@
-#setwd("/Users/morganfleming/Desktop/Phylomethods/PhyloPlasticitySals/data")
-tre<-read.nexus("~/Desktop/Phylomethods/PhyloPlasticitySals/data/amphibian_tree.nex")
-sal.data<-read.csv("~/Desktop/Phylomethods/PhyloPlasticitySals/data/salamanders.csv")
-dim(sal.data)
-unique(sal.data$Genus)
+#setwd("/Users/morganfleming/Desktop/Phylomethods/Phylo_Final_Project/data")
+tre<-read.nexus("~/Desktop/Phylomethods/Phylo_Final_Project/data/amphibian_tree.nex")
 
-sal.data$Genus[which(sal.data$Genus=='Leurognathus')]<-'Desmognathus marmoratus' 
-sal.data2 <- sal.data
-sal.data2$gen2<-gsub(' ', '_',sal.data2$Genus)
-sal.data2$gen2[which(sal.data2$gen2=='Desmognathus_monticola_')] <- 'Desmognathus_monticola'
-sal.data2$gen2[which(sal.data2$gen2=='Plethodon_cinereus_')] <- 'Plethodon_cinereus'
-sal.data2$gen2[which(sal.data2$gen2=='Eurycea_bislineata_')] <- 'Eurycea_bislineata'
-sal.data2$gen2[which(sal.data2$gen2=='Plethodon_glutinosis')] <- 'Plethodon_glutinosus'
-sal.data2<-sal.data2[!(sal.data2$gen2=="Desmognathus_ochrophaeus"),]
+sals.data<-read.csv("~/Desktop/Phylomethods/Phylo_Final_Project/data/salamanders.csv")
 
-unique(sal.data2$Genus)
+sals.data$Genus[which(sals.data$Genus=='Leurognathus')]<-'Desmognathus marmoratus' 
+salamander <- sals.data
+salamander$gen2<-gsub(' ', '_',salamander$Genus)
+salamander$gen2[which(salamander$gen2=='Desmognathus_monticola_')] <- 'Desmognathus_monticola'
+salamander$gen2[which(salamander$gen2=='Plethodon_cinereus_')] <- 'Plethodon_cinereus'
+salamander$gen2[which(salamander$gen2=='Eurycea_bislineata_')] <- 'Eurycea_bislineata'
+salamander$gen2[which(salamander$gen2=='Plethodon_glutinosis')] <- 'Plethodon_glutinosus'
+salamander<-salamander[!(salamander$gen2=="Desmognathus_ochrophaeus"),]
 
+unique(salamander$Genus)
 
+salamander<- salamander[!is.na(salamander$Elevation),]
+
+salamander <- salamander[ ,c("Genus", "Sex", "SVL", "Elevation")]
+
+# #Adults
+# salamander <-salamander[salamander$Sex == "female" | salamander$Sex == "male",]
+# 
+# str(salamander)
+# salamander$Genus <- as.factor(salamander$Genus)
+# salamander$Sex <- as.factor(salamander$Sex)
+# str(salamander)
